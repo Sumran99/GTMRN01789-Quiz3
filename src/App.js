2ef46aea8d17c596e75e0173b26605abfb72b19c
Sumran99 <89527783+Sumran99@users.noreply.github.com>
2022-09-23 01:30:27 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import Box from '@mui/material/Box';
+import { DataGrid } from '@mui/x-data-grid';
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 90 },
+  {
+    field: 'name',
+    headerName: 'Name',
+    width: 150,
+  },
+
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 110,
+    
+  },
+  {
+    field: 'city',
+    headerName: 'City',
+    width: 110,
+  },
+  
+];
+
+const rows = [
+  { id: 1, name: 'Jon', age: 35, city: "Melbourne" },
+  { id: 2, name: 'Cersei', age: 42, city: "NY" },
+  { id: 3, name: 'Jaime', age: 45, city: "Ohio" },
+  { id: 4, name: 'Arya', age: 16, city: "NY" },
+  { id: 5, name: 'Daenerys', age: 20, city: "Ohio" },
+  { id: 6, name: null, age: 150, city: "Ohio" },
+  { id: 7, name: 'Ferrara', age: 44, city: "Ohio" },
+  { id: 8, name: 'Rossini', age: 36, city: "Ohio" },
+  { id: 9, name: 'Harvey', age: 65, city: "Ohio" },
+];
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+<Box sx={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+        disableSelectionOnClick
+        experimentalFeatures={{ newEditingApi: true }}
+      />
+    </Box>
+    </>
   );
 }
 
